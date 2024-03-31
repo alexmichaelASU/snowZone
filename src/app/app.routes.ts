f5da6cc884b1ff1a6e6caa1d1f5df02301403f05
alexmichaelASU <michaelad@appstate.edu>
2024-03-31 19:56:10 -0400

    work

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { SelectedComponent } from './selected/selected.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'main', component: MainComponent},
+    { path: 'selected', component: SelectedComponent}, //may have id in path
+];
---
+++ src/app/selected/selected.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selected',
+  standalone: true,
+  imports: [],
+  templateUrl: './selected.component.html',
+  styleUrl: './selected.component.css'
+})
+export class SelectedComponent {
+
+}
