8b747b87f412556500c8cdccd563a8a7225493ea
alexmichaelASU <michaelad@appstate.edu>
2024-04-02 22:46:13 -0400

    work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component} from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { SelectedComponent } from './selected/selected.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [ RouterOutlet, RouterLink, RouterLinkActive, MainComponent, SelectedComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
