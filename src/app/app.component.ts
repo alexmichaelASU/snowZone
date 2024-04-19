3d4d7da54c8ca02281ddb2c831a472caff001aa6
alexmichaelASU <michaelad@appstate.edu>
2024-04-19 01:00:53 -0400

    front page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component} from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { SelectedComponent } from './selected/selected.component';
+import { FooterComponent } from './footer/footer.component';
+import { SecondHeaderComponent } from './second-header/second-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, RouterLink, RouterLinkActive, MainComponent, SelectedComponent],
+  imports: [ RouterOutlet, RouterLink, RouterLinkActive, 
+    MainComponent, SelectedComponent, FooterComponent, SecondHeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
