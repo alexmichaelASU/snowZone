353a7183ce2f1acd7bd013c98577411b514c0cad
alexmichaelASU <michaelad@appstate.edu>
2024-04-21 02:51:59 -0400

    material work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { SelectedComponent } from './selected/selected.component';
 import { FooterComponent } from './footer/footer.component';
 import { SecondHeaderComponent } from './second-header/second-header.component';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, RouterLink, RouterLinkActive, 
-    MainComponent, SelectedComponent, FooterComponent, SecondHeaderComponent],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MainComponent,
+    SelectedComponent,
+    FooterComponent,
+    SecondHeaderComponent,
+    MatSlideToggleModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
