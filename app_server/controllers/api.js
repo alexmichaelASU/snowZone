ee579a5d632fb6de058ae2f1b4a186cd20fb65ac
alexmichaelASU <michaelad@appstate.edu>
2024-04-23 15:06:03 -0400

    createProduct update

--- app_server/controllers/api.js
+++ app_server/controllers/api.js
@@ -29,7 +29,6 @@ exports.getProductById = async (req, res, next) => {
 //Used for post request , creates a product
 exports.createProduct = async (req, res, next) => {
     try {
-        console.log(req.file);
         // Create a new product using the request body
         const productData = req.body;
         
